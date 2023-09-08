7a7d58a142a2b3f926b45da639b1902a52c954a8
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-09-08 20:33:37 +0330

    -add image convertor & image validator

---
+++ TopLearn.Core/Security/ImageValidatore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace TopLearn.Core.Security
+{
+    public static class ImageValidatore
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            try
+            {
+                var img = System.Drawing.Image.FromStream(file.OpenReadStream());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
