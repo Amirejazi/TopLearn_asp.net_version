c1f5a731872b53da88771a71fab1c3a91f5417be
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-10-19 20:35:10 +0330

    Add latest course in home

---
+++ TopLearn.Web/Components/LatestCourseComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TopLearn.Core.Services.interfaces;
+
+namespace TopLearn.Web.Components
+{
+    public class LatestCourseComponent: ViewComponent
+    {
+        private ICourseService _courseService;
+
+        public LatestCourseComponent(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("/Views/Components/LatestCourseComponent.cshtml", _courseService.GetCourses().Item1));
+        }
+    }
+}
