8a94eaabc48cdeaeee8b6ed389ce098893fc52b8
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-10-19 20:35:38 +0330

    Add popular courses in home

---
+++ TopLearn.Web/Components/PopularCourseComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TopLearn.Core.Services.interfaces;
+
+namespace TopLearn.Web.Components
+{
+    public class PopularCourseComponent : ViewComponent
+    {
+        private ICourseService _courseService;
+
+        public PopularCourseComponent(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("/Views/Components/PopularCourseComponent.cshtml", _courseService.GetPopularCourse()));
+        }
+    }
+}
