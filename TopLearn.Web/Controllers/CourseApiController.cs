205252e5ae7a11f7774a25bbe69f7f8891fa1f28
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-10-19 20:36:46 +0330

    auto complete

---
+++ TopLearn.Web/Controllers/CourseApiController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TopLearn.DataLayer.Context;
+
+namespace TopLearn.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseApiController : ControllerBase
+    {
+        private TopLearnContext _context;
+
+        public CourseApiController(TopLearnContext context)
+        {
+            _context = context;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public async Task<IActionResult> Search()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"];
+                var courseTitle = _context.Courses
+                    .Where(c => c.CourseTitle.Contains(term))
+                    .Select(c => c.CourseTitle)
+                    .ToList();
+                return Ok(courseTitle);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
